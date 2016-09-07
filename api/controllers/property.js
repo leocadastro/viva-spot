06144aa55c8eee5df7a3252ef48589db71a2bcb9
Leonardo Cadastro <lcadastro_inf@yahoo.com.br>
2016-09-07 14:37:27 -0300

    added post roperty

--- api/controllers/property.js
+++ api/controllers/property.js
@@ -1,29 +1,69 @@
 'use strict';
 
 const mongoose = require('mongoose');
-var model = mongoose.model('Property');
+var Property = mongoose.model('Property');
 
 function getProperty(req, res, next) {
 	var id = req.swagger.params.id.value;
 
-	model
-		.findById(id)
+	Property
+		.findById(id).exec()
 		.then(function (property) {
-			console.log(property);
+
+			if (!property) {
+				res.status(404).send({
+			        message: "Property Not Found"
+			    });
+
+				return next();
+			}
+
 			res.status(200).send(property);
 
 			return next();
 
 		}, function (error) {
+			console.log(error);
 			res.status(500).send({
-		        status: 500,
-		        message: error
+		        message: error.message
 		    });
 
-			return next();
+			return next(error);
 		});
 }
 
+function postProperty(req, res, next) {
+
+	var newProperty = req.swagger.params.property.value;
+
+	if (!newProperty) {
+		res.status(500).send({
+			message: "Something wrong"
+		});
+	}
+
+	newProperty.provinces = ["TODO"];
+
+	Property
+		.create(newProperty)
+		.then(function (property) {
+			res.status(200).send({
+				success: 1,
+		        message: "property inserted with success",
+				entityId: property._id
+		    });
+		}, function (error) {
+
+			res.status(500).send({
+		        message: JSON.stringify(error)
+		    });
+
+			return next(error);
+		})
+
+}
+
 module.exports = {
-  getProperty: getProperty
+  getProperty: getProperty,
+  postProperty: postProperty
 };
