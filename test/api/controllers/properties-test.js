20d85ebd6b7a30119dd028e166793e9d3e20edc8
Leonardo Cadastro <lcadastro_inf@yahoo.com.br>
2016-09-09 19:30:29 -0300

    improving tests

--- test/api/controllers/properties-test.js
+++ test/api/controllers/properties-test.js
@@ -5,8 +5,19 @@ var validator = new ZSchema({});
 var expect = chai.expect;
 var app = require('../../../app');
 var request = require('supertest');
+const mongoose = require('mongoose');
+var Property = mongoose.model('Property');
 
 describe('/properties', function() {
+
+	before(function(done) {
+		Property.remove({}, function(err) {
+		    console.log('Properties removed before running tests')
+		 });
+	   done()
+	})
+
+
   describe('post', function() {
 
     it('should respond with 200 Success', function(done) {
@@ -48,6 +59,335 @@ describe('/properties', function() {
 		});
 
     });
+
+	it('should insert property on Gode province', function(done) {
+      /*eslint-disable*/
+      var schema = {
+        "type": "object",
+        "required": [
+          "message"
+        ],
+        "properties": {
+          "message": {
+            "type": "string",
+            "description": "a short comment"
+          },
+          "entityId": {
+            "type": "string",
+            "description": "a short comment"
+          }
+        }
+      };
+
+	  var id = "";
+
+	  request(app)
+          .post('/v1/properties')
+          .set('Accept', 'application/json')
+		  .send({
+			  "x": 222,
+			  "y": 600,
+			  "title": "Imóvel código 13, com 5 quartos e 4 banheiros",
+			  "price": 1250000,
+			  "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			  "beds": 2,
+			  "baths": 3,
+			  "squareMeters": 210
+		  })
+          .end(function(err, res) {
+			  request(app)
+  				.get('/v1/properties/' + res.body.entityId)
+  				.set('Accept', 'application/json')
+  				.end(function(err, res) {
+					expect(res.statusCode).to.equal(200);
+					expect(res.body.provinces).to.deep.equal(['Gode']);
+  					done();
+  			});
+	  });
+
+    });
+
+	it('should insert property on Ruja province', function(done) {
+      /*eslint-disable*/
+      var schema = {
+        "type": "object",
+        "required": [
+          "message"
+        ],
+        "properties": {
+          "message": {
+            "type": "string",
+            "description": "a short comment"
+          },
+          "entityId": {
+            "type": "string",
+            "description": "a short comment"
+          }
+        }
+      };
+
+	  var id = "";
+
+	  request(app)
+          .post('/v1/properties')
+          .set('Accept', 'application/json')
+		  .send({
+			  "x": 900,
+			  "y": 600,
+			  "title": "Imóvel código 13, com 5 quartos e 4 banheiros",
+			  "price": 1250000,
+			  "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			  "beds": 2,
+			  "baths": 3,
+			  "squareMeters": 210
+		  })
+          .end(function(err, res) {
+			  request(app)
+  				.get('/v1/properties/' + res.body.entityId)
+  				.set('Accept', 'application/json')
+  				.end(function(err, res) {
+					expect(res.statusCode).to.equal(200);
+					expect(res.body.provinces).to.deep.equal(['Ruja']);
+  					done();
+  			});
+	  });
+
+    });
+
+	it('should insert property on Jaby province', function(done) {
+      /*eslint-disable*/
+      var schema = {
+        "type": "object",
+        "required": [
+          "message"
+        ],
+        "properties": {
+          "message": {
+            "type": "string",
+            "description": "a short comment"
+          },
+          "entityId": {
+            "type": "string",
+            "description": "a short comment"
+          }
+        }
+      };
+
+	  var id = "";
+
+	  request(app)
+          .post('/v1/properties')
+          .set('Accept', 'application/json')
+		  .send({
+			  "x": 1300,
+			  "y": 600,
+			  "title": "Imóvel código 13, com 5 quartos e 4 banheiros",
+			  "price": 1250000,
+			  "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			  "beds": 2,
+			  "baths": 3,
+			  "squareMeters": 210
+		  })
+          .end(function(err, res) {
+			  request(app)
+  				.get('/v1/properties/' + res.body.entityId)
+  				.set('Accept', 'application/json')
+  				.end(function(err, res) {
+					expect(res.statusCode).to.equal(200);
+					expect(res.body.provinces).to.deep.equal(['Jaby']);
+  					done();
+  			});
+	  });
+
+    });
+
+	it('should insert property on Scavy province', function(done) {
+      /*eslint-disable*/
+      var schema = {
+        "type": "object",
+        "required": [
+          "message"
+        ],
+        "properties": {
+          "message": {
+            "type": "string",
+            "description": "a short comment"
+          },
+          "entityId": {
+            "type": "string",
+            "description": "a short comment"
+          }
+        }
+      };
+
+	  var id = "";
+
+	  request(app)
+          .post('/v1/properties')
+          .set('Accept', 'application/json')
+		  .send({
+			  "x": 222,
+			  "y": 100,
+			  "title": "Imóvel código 13, com 5 quartos e 4 banheiros",
+			  "price": 1250000,
+			  "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			  "beds": 2,
+			  "baths": 3,
+			  "squareMeters": 210
+		  })
+          .end(function(err, res) {
+			  request(app)
+  				.get('/v1/properties/' + res.body.entityId)
+  				.set('Accept', 'application/json')
+  				.end(function(err, res) {
+					expect(res.statusCode).to.equal(200);
+					expect(res.body.provinces).to.deep.equal(['Scavy']);
+  					done();
+  			});
+	  });
+
+    });
+
+	it('should insert property on Nova province', function(done) {
+      /*eslint-disable*/
+      var schema = {
+        "type": "object",
+        "required": [
+          "message"
+        ],
+        "properties": {
+          "message": {
+            "type": "string",
+            "description": "a short comment"
+          },
+          "entityId": {
+            "type": "string",
+            "description": "a short comment"
+          }
+        }
+      };
+
+	  var id = "";
+
+	  request(app)
+          .post('/v1/properties')
+          .set('Accept', 'application/json')
+		  .send({
+			  "x": 900,
+			  "y": 100,
+			  "title": "Imóvel código 13, com 5 quartos e 4 banheiros",
+			  "price": 1250000,
+			  "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			  "beds": 2,
+			  "baths": 3,
+			  "squareMeters": 210
+		  })
+          .end(function(err, res) {
+			  request(app)
+  				.get('/v1/properties/' + res.body.entityId)
+  				.set('Accept', 'application/json')
+  				.end(function(err, res) {
+					expect(res.statusCode).to.equal(200);
+					expect(res.body.provinces).to.deep.equal(['Nova']);
+  					done();
+  			});
+	  });
+
+    });
+
+	it('should insert property on Groola province', function(done) {
+	  /*eslint-disable*/
+	  var schema = {
+		"type": "object",
+		"required": [
+		  "message"
+		],
+		"properties": {
+		  "message": {
+			"type": "string",
+			"description": "a short comment"
+		  },
+		  "entityId": {
+			"type": "string",
+			"description": "a short comment"
+		  }
+		}
+	  };
+
+	  var id = "";
+
+	  request(app)
+		  .post('/v1/properties')
+		  .set('Accept', 'application/json')
+		  .send({
+			  "x": 700,
+			  "y": 100,
+			  "title": "Imóvel código 13, com 5 quartos e 4 banheiros",
+			  "price": 1250000,
+			  "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			  "beds": 2,
+			  "baths": 3,
+			  "squareMeters": 210
+		  })
+		  .end(function(err, res) {
+			  request(app)
+				.get('/v1/properties/' + res.body.entityId)
+				.set('Accept', 'application/json')
+				.end(function(err, res) {
+					expect(res.statusCode).to.equal(200);
+					expect(res.body.provinces).to.deep.equal(['Groola']);
+					done();
+			});
+	  });
+
+	});
+
+	it('should insert property on Gode and Ruja provinces', function(done) {
+	  /*eslint-disable*/
+	  var schema = {
+		"type": "object",
+		"required": [
+		  "message"
+		],
+		"properties": {
+		  "message": {
+			"type": "string",
+			"description": "a short comment"
+		  },
+		  "entityId": {
+			"type": "string",
+			"description": "a short comment"
+		  }
+		}
+	  };
+
+	  var id = "";
+
+	  request(app)
+		  .post('/v1/properties')
+		  .set('Accept', 'application/json')
+		  .send({
+			  "x": 550,
+			  "y": 550,
+			  "title": "Imóvel código 13, com 5 quartos e 4 banheiros",
+			  "price": 1250000,
+			  "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			  "beds": 2,
+			  "baths": 3,
+			  "squareMeters": 210
+		  })
+		  .end(function(err, res) {
+			  request(app)
+				.get('/v1/properties/' + res.body.entityId)
+				.set('Accept', 'application/json')
+				.end(function(err, res) {
+					expect(res.statusCode).to.equal(200);
+					expect(res.body.provinces).to.deep.equal(['Gode', 'Ruja']);
+					done();
+			});
+	  });
+
+	});
 
     it('should respond with default Error', function(done) {
       /*eslint-disable*/
